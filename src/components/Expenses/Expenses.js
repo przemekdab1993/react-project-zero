4c8fb1a2db0219cde67ce01c0291bc7d0764a940
przemekdab1993 <przemekdab1993@gmail.com>
2022-07-03 19:28:31 +0200

    przerobienie listy wydatków na listę dynamiczną

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,21 +25,16 @@ const Expenses = (props) => {
                 onChangeFilter={selectFilter}
                 selected={filteredYear}
             />
-            <ExpenseItem
-                title={props.expenses[0].title}
-                date={props.expenses[0].date}
-                price={props.expenses[0].price}
-            />
-            <ExpenseItem
-                title={props.expenses[1].title}
-                date={props.expenses[1].date}
-                price={props.expenses[1].price}
-            />
-            <ExpenseItem
-                title={props.expenses[2].title}
-                date={props.expenses[2].date}
-                price={props.expenses[2].price}
-            />
+
+            { props.expenses.map( (expense) => {
+                return (
+                    <ExpenseItem
+                        title={expense.title}
+                        date={expense.date}
+                        price={expense.price}
+                    />
+                )
+            }) }
         </Card>
     );
 }
