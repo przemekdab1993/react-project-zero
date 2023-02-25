2537ad823a8eb5c550b3cd80bce71899178f2ef8
przemekdab1993 <przemekdab1993@gmail.com>
2023-02-25 01:33:59 +0100

    useEffect opużnienie działania za pomocą setTimeout

--- src/components/User/LoginPanel/LoginForm.js
+++ src/components/User/LoginPanel/LoginForm.js
@@ -12,24 +12,35 @@ const LoginForm = (props) => {
 
     const [enteredUserEmail, setEnteredUserEmail] = useState('');
     const [enteredUserPassword, setEnteredUserPassword] = useState('');
-    const [emailIsValid, setEmailIsValid] = useState(true);
-    const [passwordIsValid, setPasswordIsValid] = useState(true);
+    const [emailIsValid, setEmailIsValid] = useState();
+    const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
-    useEffect(() => {
-        setFormIsValid(enteredUserEmail.includes('@') && enteredUserEmail.length >= 6 && enteredUserPassword.length >= 6);
-    },[enteredUserEmail, enteredUserPassword]);
+    useEffect( () => {
+        const checkValidEmail = setTimeout(() => {
+            setFormIsValid(enteredUserEmail.includes('@') && enteredUserEmail.length >= 6 && enteredUserPassword.length >= 6);
+            if (emailIsValid !== undefined || enteredUserEmail.length > 0) {
+                setEmailIsValid(enteredUserEmail.includes('@') && enteredUserEmail.length >= 6);
+            }
+            if (passwordIsValid !== undefined || enteredUserPassword.length > 0) {
+                setPasswordIsValid(enteredUserPassword.length >= 6);
+            }
+        },600);
+
+        return () => {
+            clearTimeout(checkValidEmail);
+        }
+    }, [enteredUserEmail, enteredUserPassword]);
+
 
     const inputChangeHandler = (event) => {
         const inputName = event.target.name;
 
         if (inputName === 'userEmail') {
             setEnteredUserEmail(prevState => event.target.value);
-            setEmailIsValid(prevState => true);
         }
         if (inputName === 'userPassword') {
             setEnteredUserPassword(prevState => event.target.value);
-            setPasswordIsValid(prevState => true);
         }
     }
 
@@ -37,17 +48,13 @@ const LoginForm = (props) => {
         const inputName = event.target.name;
 
         if (inputName === 'userEmail') {
-            if (!enteredUserEmail.includes('@') || enteredUserEmail.length < 6 ) {
+            if (enteredUserEmail.length === 0 ) {
                 setEmailIsValid(prevState => false);
-            } else {
-                setEmailIsValid(prev => true);
             }
         }
         if (inputName === 'userPassword') {
-            if (enteredUserPassword.length < 6 ) {
+            if (enteredUserPassword.length === 0 ) {
                 setPasswordIsValid(prevState => false);
-            } else {
-                setPasswordIsValid(prevState => true);
             }
         }
     }
@@ -63,9 +70,9 @@ const LoginForm = (props) => {
     return (
         <form onSubmit={submitHandler} className={styles["login-form"]}>
             <div className={`${styles["login-form-group"]}`} >
-                <label className={`${styles["login-form-label"]} ${!emailIsValid ? styles["invalid"] : ''}`} htmlFor="userEmail">E-mail:</label>
+                <label className={`${styles["login-form-label"]} ${(emailIsValid === false) ? styles["invalid"] : ''}`} htmlFor="userEmail">E-mail:</label>
                 <input
-                    className={`${!emailIsValid ? styles["invalid"] : ''}`}
+                    className={`${(emailIsValid === false) ? styles["invalid"] : ''}`}
                     type="text"
                     name="userEmail"
                     value={enteredUserEmail}
@@ -74,9 +81,9 @@ const LoginForm = (props) => {
                 />
             </div>
             <div className={`${styles["login-form-group"]}`} >
-                <label className={`${styles["login-form-label"]} ${!passwordIsValid ? styles["invalid"] : ''}`} htmlFor="userPassword">Password:</label>
+                <label className={`${styles["login-form-label"]} ${(passwordIsValid === false) ? styles["invalid"] : ''}`} htmlFor="userPassword">Password:</label>
                 <input
-                    className={`${!passwordIsValid ? styles["invalid"] : ''}`}
+                    className={`${(passwordIsValid === false) ? styles["invalid"] : ''}`}
                     type="password"
                     name="userPassword"
                     value={enteredUserPassword}
